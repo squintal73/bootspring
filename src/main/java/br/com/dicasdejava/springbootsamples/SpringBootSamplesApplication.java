60841fde7d96fcc96fed47d531176226535998c7
squintal <sidnei_quintal@yahoo.com.br>
2021-03-29 18:36:07 -0300

    Primeira etapa do bootspring - Configurações inicias

---
+++ src/main/java/br/com/dicasdejava/springbootsamples/SpringBootSamplesApplication.java
@@ -0,0 +1,13 @@
+package br.com.dicasdejava.springbootsamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dicasdejava/springbootsamples/SpringBootSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dicasdejava.springbootsamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
